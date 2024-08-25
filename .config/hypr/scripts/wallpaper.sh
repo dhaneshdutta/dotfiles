18bdee1b9144b76e411ee9ad2a2f72f12a84b7d7
dhaneshdutta <dhanesh1551@gmail.com>
2024-08-25 10:56:21 +0530

    waybar changes

--- .config/hypr/scripts/wallpaper.sh
+++ .config/hypr/scripts/wallpaper.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 commands=(
-  "wal -i Pictures/Wallpapers/Earth.jpg & killall waybar &"
-  "wal -i Pictures/Wallpapers/BerserkS.jpg & killall waybar &"
-  "wal -i Pictures/Wallpapers/Berserk.jpg & killall waybar &"
-  "wal -i Pictures/Wallpapers/Japan.jpeg & killall waybar &"  
+  "wal -i Pictures/Wallpapers/Earth.jpg"
+  "wal -i Pictures/Wallpapers/BerserkS.jpg"
+  "wal -i Pictures/Wallpapers/Berserk.jpg"
+  "wal -i Pictures/Wallpapers/Japan.jpeg"
+  "wal -i Pictures/Wallpapers/Sekiro.jpg"
+  "wal -i Pictures/Wallpapers/Sekiro1.jpg"  
+  "wal -i Pictures/Wallpapers/Sekiro2.png"
 )
 
-index_file="/home/$USER/.config/hypr/scripts/.wallpaper_index"
+index_file="/home/dhanesh/.config/hypr/scripts/.wallpaper_index"
 
 if [ -f "$index_file" ]; then
   current_index=$(cat "$index_file")
@@ -16,6 +19,11 @@ else
 fi
 
 eval "${commands[$current_index]}"
+
+sleep 1
+
+killall waybar
+waybar &
 
 next_index=$(( (current_index + 1) % ${#commands[@]} ))
 
