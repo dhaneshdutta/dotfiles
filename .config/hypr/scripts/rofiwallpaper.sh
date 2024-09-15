b0244c0a853c574bf0565a8b891f745cf5cb4b80
dhaneshdutta <dhanesh1551@gmail.com>
2024-09-16 02:42:01 +0530

    added some scripts

---
+++ .config/hypr/scripts/rofiwallpaper.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+WALLPAPER_DIR="$HOME/Pictures/Wallpapers"
+CACHE_DIR="$HOME/.cache/wallpaper-rofi"
+
+mkdir -p "$CACHE_DIR"
+
+generate_menu() {
+    for img in "$WALLPAPER_DIR"/*.{jpg,jpeg,png}; do
+        filename=$(basename "$img")
+        cached_thumb="$CACHE_DIR/${filename%.*}.png"
+        
+        if [ ! -f "$cached_thumb" ]; then
+            convert "$img" -thumbnail 200x200^ -gravity center -extent 200x200 "$cached_thumb"
+        fi
+        
+        echo -en "$filename\0icon\x1f$cached_thumb\n"
+    done
+}
+
+change_wallpaper() {
+    local wallpaper="$WALLPAPER_DIR/$1"
+    
+    # Change wallpaper with swww
+    swww img "$wallpaper" --transition-type grow --transition-pos 1,1 --transition-duration 1.5 --transition-fps 60 --transition-angle 60
+    
+    # Generate color scheme with pywal
+    wal -i "$wallpaper" -n
+    
+    # Reload Rofi theme
+    pkill -USR1 rofi
+}
+
+main() {
+    # Source pywal colors
+    . "$HOME/.cache/wal/colors.sh"
+    
+    chosen=$(generate_menu | rofi -dmenu -i -p "Select Wallpaper" -theme ~/.config/rofi/wallpaper-theme.rasi)
+    
+    if [ -n "$chosen" ]; then
+        change_wallpaper "$chosen"
+    fi
+}
+
+main
