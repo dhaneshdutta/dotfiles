84596ee62b3810496d17141e9479e2d7e4cf8b17
dhaneshdutta <dhanesh1551@gmail.com>
2024-09-21 02:50:43 +0530

    lgtm

--- .config/hypr/scripts/move.sh
+++ .config/hypr/scripts/move.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /usr/share/applications
-sudo mv libreoffice-base.desktop libreoffice-calc.desktop libreoffice-draw.desktop libreoffice-impress.desktop libreoffice-math.desktop libreoffice-startcenter.desktop libreoffice-writer.desktop libreoffice-xsltfilter.desktop nvim.desktop vim.desktop electron30.desktop qemu.desktop io.elementary.granite.demo.desktop cmake-gui.desktop rofi.desktop rofi-theme-selector.desktop wine.desktop /home/dhanesh/Desktop/
+sudo mv libreoffice-base.desktop libreoffice-calc.desktop libreoffice-draw.desktop libreoffice-impress.desktop libreoffice-math.desktop libreoffice-startcenter.desktop libreoffice-writer.desktop libreoffice-xsltfilter.desktop nvim.desktop vim.desktop electron30.desktop qemu.desktop io.elementary.granite.demo.desktop cmake-gui.desktop rofi.desktop rofi-theme-selector.desktop wine.desktop scrcpy.desktop sxiv.desktop emote.desktop gcr-prompter.desktop gcr-viewer.desktop scrcpy-console.desktop mpv.desktop fish.desktop org.pwmt.zathura-cb.desktop org.pwmt.zathura.desktop org.pwmt.zathura-pdf-mupdf.desktop org.pwmt.zathura-djvu.desktop org.pwmt.zathura-ps.desktop /home/dhanesh/Desktop/
